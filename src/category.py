563858a658708481dd85118a0adc90cba6d02226
nnikitenko <nnikitenko@aorti.ru>
2024-03-11 15:40:17 +0300

    Написаны тесты. Отредактирован README.md

--- src/category.py
+++ src/category.py
@@ -7,11 +7,14 @@ class Category:
     description: str
     products: list
     category_count: int = 0
-    uniq_products_count: int = 0
+    uniq_products_count: int
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = copy.deepcopy(products)
         Category.category_count += 1
-        Category.uniq_products_count = len(set(products))
+        self.uniq_products_count = len(set(products))
+
+    def __del__(self):
+        Category.category_count -= 1
