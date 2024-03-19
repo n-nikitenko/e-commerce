763fc197f81f44d3b126c18953e74f7ce8b8eb6f
nnikitenko <ninanineliya@gmail.com>
2024-03-19 21:02:10 +0300

    1. Для класса Category  список товаров сделан приватным атрибутом. Для добавления товаров в категорию реализован метод add_product. 2. Для атрибута класса Category  «товары» добавлен геттер, который выводит список товаров на консоль. 3. Для класса Product добавлен метод create_product, который создает товар и возвращает объект, который можно добавлять в список товаров. Реализована проверка наличия такого же товара, схожего по имени в переданной категории  товаров, в которой нужно искать дубликаты. 4. Для класса Product описан геттер и сеттер для атрибута цены. В случае если цена равна или ниже нуля, выводится  сообщение в консоль, что цена введена некорректная, при этом новая цена не устанавливается. Добавлена логика подтверждения пользователем в случае, если цена понижается. 5. Для класса Category реализовано добавление товара в список товаров с учетом возможных дубликатов.

--- src/product.py
+++ src/product.py
@@ -2,14 +2,40 @@ class Product:
     """Товар"""
     name: str
     description: str
-    price: float
+    __price: float
     count: int
 
     def __init__(self, name, description, price, count):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.count = count
 
     def __repr__(self):
         return f"Product(name='{self.name}', description='{self.description}', price={self.price}, count={self.count})"
+
+    @classmethod
+    def create_product(cls, name, description, price, count, category):
+        """Проверяет наличие схожего товара в категории и возвращает созданный товар"""
+        duplicates = [p for p in category.products if p.name.lower() == name.lower()]
+        for p in duplicates:
+            count += p.count
+            price = max(price, p.price)
+        return cls(name, description, price, count)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value: float):
+        '''Устанавливает новое значение цены, если оно больше нуля. Если цена понижается, запрашивается подтверждение
+        пользователя'''
+        if value <= 0:
+            print("Значение цены должно быть больше нуля")
+            return
+        if value < self.__price:
+            user_answer = input("Вы уверены, что хотите понизить цену? Введите y/n")
+            if user_answer.lower() != 'y':
+                return
+        self.__price = value
